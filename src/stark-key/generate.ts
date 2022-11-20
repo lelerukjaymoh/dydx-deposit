508df74962b5829c7c4dcbb0b5a72ce8627b0fd0
lelerukjaymoh <lelerukjaymoh@gmail.com>
2022-11-20 18:01:03 +0100

    Implement deposit to dydx

--- src/stark-key/generate.ts
+++ src/stark-key/generate.ts
@@ -1,16 +1,19 @@
 
-import { DydxClient } from "@dydxprotocol/v3-client";
-import { ethers } from 'ethers'
+import { DydxClient, SignEthPrivateAction, SigningMethod } from "@dydxprotocol/v3-client";
+import { ethers, utils } from 'ethers'
 import { WebSocket } from 'ws'
 import Web3 from "web3"
 import { deposit } from "../deposit/deposit";
-import { toHex } from "../utils/common";
+import { RequestMethod } from "@dydxprotocol/v3-client/build/src/lib/axios";
 
 
 const web3: any = new Web3(process.env.NODE_URL!)
 web3.eth.accounts.wallet.add(process.env.PRIVATE_KEY);
 
 let accountDetails;
+const client = new DydxClient(process.env.DYDX_HTTP_HOST!, { web3 })
+let localSigner = new SignEthPrivateAction(web3, 1);
+
 
 const getApiCred = async (client: DydxClient) => {
 
@@ -20,12 +23,14 @@ const getApiCred = async (client: DydxClient) => {
     const apiCreds = await client.onboarding.recoverDefaultApiCredentials(ethers.utils.getAddress(process.env.WALLET_ADDRESS!))
     client.apiKeyCredentials = apiCreds
 
+
+    console.log("Creds ", apiCreds)
+
     return apiCreds
 
 }
 
 const getApiCredentials = async () => {
-    const client = new DydxClient(process.env.DYDX_HTTP_HOST!, { web3 })
 
     const apiCreds = await getApiCred(client)
     const method: any = "GET"
@@ -42,6 +47,8 @@ const getApiCredentials = async () => {
         timestamp,
         apiCreds
     }
+
+    // console.log(data)
 
     return data
 }
@@ -67,14 +74,58 @@ const fetchAccountDetails = async () => {
         ws.on('message', async (message: string) => {
             const response = JSON.parse(message)
 
+            console.log("response ", response)
+
+            const request = {
+                requestPath: '/ws/accounts',
+                method: RequestMethod.GET,
+                body: '{}',
+                timestamp: new Date().toISOString()
+            }
+
+            // const mockRequestNoBody = {
+            //     requestPath: 'v3/test',
+            //     method: ApiMethod.POST,
+            //     body: '{}',
+            //     timestamp: '2021-01-08T10:06:12.500Z',
+            // };
+
+            // const signature = await localSigner.sign(
+            //     GANACHE_ADDRESS,
+            //     SigningMethod.Hash,
+            //     mockRequestNoBody,
+            // );
+
+            // Generate the signature
+            const signature = await localSigner.sign(
+                process.env.WALLET_ADDRESS!,
+                SigningMethod.Hash,
+                request,
+            );
+
+            // const starkKey = deriveStarkKey()
+
+            // console.log("Signature ", signature)
+
+
+            const starkkey = await client.onboarding.deriveStarkKey(process.env.l2!)
+            console.log("Starkkey ", starkkey)
+
+            const client_account_info = await client.private.getAccount(process.env.WALLET_ADDRESS!);
+
+            console.log("Clienc ", client_account_info)
+
+            console.log(await client.starkPrivateKey)
+
+
             if (response.type == "subscribed") {
                 accountDetails = response.contents.account
-                console.log(accountDetails)
+                console.log("Account details ", accountDetails)
 
-                const depositAmount = 20 * 1e18
+                const depositAmount = utils.parseUnits("1", 6)
 
                 // deposit
-                await deposit(toHex(depositAmount), accountDetails.starkKey, accountDetails.positionId, creds.signature)
+                await deposit(depositAmount, accountDetails.starkKey.toString(), accountDetails.positionId, signature)
             }
 
         })
--- test/test.ts
+++ test/test.ts
@@ -1,10 +1,53 @@
+import { DydxClient, SignEthPrivateAction } from "@dydxprotocol/v3-client";
+import Onboarding from "@dydxprotocol/v3-client/build/src/modules/onboarding";
 import axios from "axios"
+import Web3 from "web3";
+import { utils } from "ethers"
+
+const web3: any = new Web3(process.env.NODE_URL!)
+web3.eth.accounts.wallet.add(process.env.PRIVATE_KEY);
+
+
+const client = new DydxClient(process.env.DYDX_HTTP_HOST!, { web3 })
+let localSigner = new SignEthPrivateAction(web3, 1);
+
 
 const getData = async () => {
     try {
-        const data = await axios.post("https://api.dydx.exchange/v3/accounts/")
 
-        console.log(data.data)
+
+        const apiCreds = await client.onboarding.recoverDefaultApiCredentials(utils.getAddress(process.env.WALLET_ADDRESS!))
+        client.apiKeyCredentials = apiCreds
+
+
+        console.log(await client.eth.exchange.getProxyDepositAddress())
+
+        // console.log("Client ", await client)
+
+
+        const approve = await client.eth.exchange.setERC20Allowance({
+            tokenAddress: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
+            address: "0x8e8bd01b5A9eb272CC3892a2E40E64A716aa2A40",
+            amount: "115792089237316195423570985008687907853269984665640564039457584007913129639935"
+        },
+            //     {
+            //         from: client_account_info.account.,
+            //    .
+        )
+
+        console.log("Appoval ", approve)
+
+        const client_account_info = await client.private.getAccount(process.env.WALLET_ADDRESS!);
+
+        const tx = await client.eth.exchange.deposit({
+            starkKey: client_account_info.account.starkKey,
+            positionId: client_account_info.account.positionId,
+            humanAmount: 0.001
+        })
+
+
+        console.log("Tx ", tx)
+
     } catch (error) {
         console.log("Error : ", error)
     }
